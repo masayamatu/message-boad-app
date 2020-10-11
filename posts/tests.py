112e12f76ea671004e6cdbfab8916437572902c1
宮田将義 <masayamatu@miyatasusumuginoMacBook-puro.local>
2020-10-11 18:39:45 +0900

    test change

--- posts/tests.py
+++ posts/tests.py
@@ -15,12 +15,12 @@ class HomePageViewTest(TestCase):
     def test_view_url_exists_at_proper_location(self):
         resp = self.client.get('/')
         self.assertEqual(resp.status_code,200)
-def test_view_url_by_name(self):
-    resp = self.client.get(reverse('home'))
-    self.assertEqual(resp.status_code,200)
-def test_view_uses_correct_template(self):
-    resp = self.client.get(reverse('home'))
-    self.assertEqual(resp.status_code,200)
-    self.assertTemplateUsed(resp,'home.html')
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code,200)
+    def test_view_uses_correct_template(self):
+        resp = self.client.get(reverse('home'))
+        self.assertEqual(resp.status_code,200)
+        self.assertTemplateUsed(resp,'home.html')
 
 # Create your tests here.
